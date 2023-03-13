c0175002c5e445314eb825ebb897c2a5e3e2c173
Christian Razo <37232875+Christian-Ra@users.noreply.github.com>
2023-03-13 15:52:34 -0500

    Completed Excercises 1.1, refactoring base app

--- Part_1/exercises/course_information/src/App.js
+++ Part_1/exercises/course_information/src/App.js
@@ -1,3 +1,32 @@
+const Header = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  );
+};
+
+const Content = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <p>
+        {props.exercise} {props.count}
+      </p>
+    </div>
+  );
+};
+
+const Total = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <p>Number of excercises {props.total}</p>
+    </div>
+  );
+};
+
 const App = () => {
   const course = "Half Stack application development";
   const part1 = "Fundamentals of React";
@@ -9,17 +38,11 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course}></Header>
+      <Content exercise={part1} count={exercises1} />
+      <Content exercise={part2} count={exercises2} />
+      <Content exercise={part3} count={exercises3} />
+      <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   );
 };
