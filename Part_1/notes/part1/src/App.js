d2fb5ade49ead9c3942b507fd670d2a163b11216
Christian Razo <37232875+Christian-Ra@users.noreply.github.com>
2023-03-17 13:46:51 -0500

    Setup code for Part 1.c notes and material

--- Part_1/notes/part1/src/App.js
+++ Part_1/notes/part1/src/App.js
@@ -1,25 +1,28 @@
-const Hello = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old!
-      </p>
-    </div>
-  );
-};
+// const Hello = (props) => {
+//   console.log(props);
+//   return (
+//     <div>
+//       <p>
+//         Hello {props.name}, you are {props.age} years old!
+//       </p>
+//     </div>
+//   );
+// };
 
-const App = () => {
-  const name = "Peter";
-  const age = 10;
-  return (
-    <div>
-      <p>Greetings</p>
-      <Hello name="Chris" age={26 - 4} />
-      <Hello name={name} age={age} />
-      <Hello name="Crusty" />
-    </div>
-  );
+const App = (props) => {
+  // const name = "Peter";
+  // const age = 10;
+  // return (
+  //   <div>
+  //     <p>Greetings</p>
+  //     <Hello name="Chris" age={26 - 4} />
+  //     <Hello name={name} age={age} />
+  //     <Hello name="Crusty" />
+  //   </div>
+  // );
+
+  const { counter } = props;
+  return <div>{counter}</div>;
 };
 
 export default App;
