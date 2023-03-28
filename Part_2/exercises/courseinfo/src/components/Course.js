d96504ca04f81d53b66428293cd96620880810f8
Christian Razo <37232875+Christian-Ra@users.noreply.github.com>
2023-03-28 09:51:54 -0500

    Completed Exercises 2.1 - 2.6
    Courses can display dynamic data such as multiple courses using map and reduce methods

--- Part_2/exercises/courseinfo/src/components/Course.js
+++ Part_2/exercises/courseinfo/src/components/Course.js
@@ -1,34 +1,59 @@
-const Course = ({ course }) => {
-  const parts = course.parts;
+const Course = ({ course }, key) => {
+  const parts = course.parts; //Destructure parts array to make mapping easier
   console.log("list of props, ", course);
   console.log("Parts of Course: ", parts);
+
+  const getExercises = (parts) => {
+    const initial = 0;
+    let total = parts.reduce(
+      (accumulator, current) => accumulator + current.exercises,
+      initial //initialized variable needed, otherwise starts storing the data as strings
+    );
+    // parts.forEach((part) => {
+    //   total += part.exercises;
+    // });
+
+    // for (let i = 0; i < parts.length; i++) {
+    //   console.log(parts.exercises);
+    //   total = total + parts[i].exercises;
+    // }
+    return total;
+  };
 
   const Header = ({ title }) => {
     return <h1>{title.name}</h1>;
   };
 
-  const Total = ({ sum }) => <p>Number of exercises {sum}</p>;
+  const Total = ({ sum }) => <b>Number of exercises {sum}</b>;
 
-  const Part = ({ part }) => (
-    <p>
+  const Part = (
+    { part },
+    key //!key should not be listed as a prop, console returns undefined
+  ) => (
+    <li key={key}>
+      {" "}
       {part.name} {part.exercises}
-    </p>
+    </li>
   );
 
   const Content = ({ partsList }) => (
-    <>
-      <Part part={partsList[0]} />
-      <Part part={partsList[1]} />
-      <Part part={partsList[2]} />
-    </>
+    <ul>
+      {partsList.map((part) => (
+        <Part part={part} key={part.id}></Part>
+      ))}
+    </ul>
+
+    // <>
+    //   <Part part={partsList[0]} />
+    //   <Part part={partsList[1]} />
+    //   <Part part={partsList[2]} />
+    // </>
   );
   return (
     <div>
       <Header title={course} />
       <Content partsList={parts} />
-      <Total
-        sum={parts[0].exercises + parts[1].exercises + parts[2].exercises}
-      />
+      <Total sum={getExercises(parts)} />
     </div>
   );
 };
