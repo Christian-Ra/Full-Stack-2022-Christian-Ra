dbc4512a3266832365461e031ce463fcd37c2e24
Christian Razo <37232875+Christian-Ra@users.noreply.github.com>
2023-03-31 11:13:47 -0500

    Completed notes for Part2.C Getting data from the server

--- Part_2/notes/collections/src/App.js
+++ Part_2/notes/collections/src/App.js
@@ -1,14 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Note from "./components/Note";
 
 //best place to store notes is in
 //the App components state
 
-const App = (props) => {
+const App = () => {
   // const [notes, setNotes] = useState([]); if state initially empty, no need to pass props
-  const [notes, setNotes] = useState(props.notes);
+  const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
+
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/notes").then((response) => {
+      console.log("promise fulfilled");
+      setNotes(response.data);
+    });
+  }, []);
+  console.log("render", notes.length, "notes");
 
   const handleNoteChange = (event) => {
     console.log(event.target.value);
--- Part_2/notes/collections/src/index.js
+++ Part_2/notes/collections/src/index.js
@@ -3,24 +3,40 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 
-const notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
-];
+import axios from "axios";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <App notes={notes} />
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+
+// axios.get("http://localhost:3001/notes").then((response) => {
+//   const notes = response.data;
+//   ReactDOM.createRoot(document.getElementById("root")).render(
+//     <App notes={notes} />
+//   );
+// });
+//! ^^^ This method has many issues, as we're rendering the entire App component
+//! ^^^ Only when we successfully retrieve a response
+
+// const promise = axios.get("http://localhost:3001/notes");
+// console.log(promise);
+// promise.then((response) => {
+//   console.log(response);
+// });
+
+//* vvvv Part.A notes, rendering a collection, modules
+// const notes = [
+//   {
+//     id: 1,
+//     content: "HTML is easy",
+//     important: true,
+//   },
+//   {
+//     id: 2,
+//     content: "Browser can execute only JavaScript",
+//     important: false,
+//   },
+//   {
+//     id: 3,
+//     content: "GET and POST are the most important methods of HTTP protocol",
+//     important: true,
+//   },
+// ];
