96e5717fe780108e0911751638435d2abb47b7b7
Christian Razo <37232875+Christian-Ra@users.noreply.github.com>
2023-04-05 12:45:09 -0500

    Basic styles, improved error messages
    Part 2.e adding styles to react app

--- Part_2/notes/collections/src/App.js
+++ Part_2/notes/collections/src/App.js
@@ -1,15 +1,34 @@
 import { useState, useEffect } from "react";
 import noteService from "./services/notes"; //can extract backend communication into a seperate module
 import Note from "./components/Note";
+import Notification from "./components/Notification";
+import "./index.css";
 
 //best place to store notes is in
 //the App components state
+
+const Footer = () => {
+  const footerStyle = {
+    color: "green",
+    fontStyle: "italic",
+    fontSize: 16,
+  };
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>
+        Note app, Department of Comuter Science, University of Helsinki 2023
+      </em>
+    </div>
+  );
+};
 
 const App = () => {
   // const [notes, setNotes] = useState([]); if state initially empty, no need to pass props
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState("some error happened...");
 
   //* vvv Following function is executed immediately after rendering. The executions of the function results in
   //* effect being printed to the console, and the command axios.get initiates the fecthing of the data from
@@ -32,7 +51,15 @@ const App = () => {
         setNotes(notes.map((n) => (n.id !== id ? n : returnedNote)));
       })
       .catch((error) => {
-        alert(`the note '${note.content}' was already deleted from server`);
+        // alert(`the note '${note.content}' was already deleted from server`);
+        // setNotes(notes.filter((n) => n.id !== id));
+
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        );
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
         setNotes(notes.filter((n) => n.id !== id));
       });
   };
@@ -47,7 +74,6 @@ const App = () => {
     const noteObject = {
       content: newNote,
       important: Math.random() < 0.5,
-      // id: notes.length + 1,S
     };
     noteService.create(noteObject).then((returnedNote) => {
       setNotes(notes.concat(returnedNote));
@@ -61,6 +87,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? "important" : "all"}
@@ -79,6 +106,7 @@ const App = () => {
         <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
       </form>
+      <Footer />
     </div>
   );
 };
