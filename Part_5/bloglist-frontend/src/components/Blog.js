5f5dad1bc5952f02e717b2487968ef914c638f9b
Christian Razo <37232875+Christian-Ra@users.noreply.github.com>
2023-06-28 18:32:54 -0500

    Added styles and detailed view to blogPosts

--- Part_5/bloglist-frontend/src/components/Blog.js
+++ Part_5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,36 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} : {blog.author}
-  </div>
-);
+import { useState } from "react";
+
+const Blog = ({ blog }) => {
+  const [blogView, toggleBlogView] = useState(false);
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    verticalAlign: "center",
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+  return (
+    <div>
+      <div style={blogStyle}>
+        {blog.title} : {blog.author}
+        <br></br>
+        <button onClick={() => toggleBlogView(!blogView)}>
+          {blogView ? "Hide" : "Show"}
+        </button>
+        {blogView && (
+          <div>
+            {blog.url}
+            <p>
+              Likes {blog.likes}
+              <button onClick={() => console.log("Click!")}>Like</button>
+            </p>
+            {blog.user.name}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default Blog;
