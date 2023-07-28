d67899ba3342f06813e3d3ffaf3b4a8a19e794a9
Christian Razo <37232875+Christian-Ra@users.noreply.github.com>
2023-07-27 20:21:06 -0500

    added asynchronous actions with redux thunk

--- Part_6/notes/notes-redux-app/src/index.js
+++ Part_6/notes/notes-redux-app/src/index.js
@@ -1,32 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 // import { createStore, combineReducers } from "redux"; --no longer needed with redux toolkit
-import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
+import store from "./store";
 
 // import { createNote } from "./reducers/noteReducer";
 // import { filterChange } from "./reducers/filterReducer";
 import App from "./App";
-
-import noteService from "./services/notes";
-import noteReducer, { setNotes } from "./reducers/noteReducer";
-import filterReducer from "./reducers/filterReducer";
 
 // const reducer = combineReducers({
 //   notes: noteReducer,
 //   filter: filterReducer,
 // });
 // const store = createStore(reducer);
-
-const store = configureStore({
-  reducer: {
-    notes: noteReducer,
-    filter: filterReducer,
-  },
-});
-
-console.log(store.getState());
-noteService.getAll().then((notes) => store.dispatch(setNotes(notes)));
 
 //* Below code is a bit impractical, since multiple action calls are required
 // noteService.getAll().then((notes) =>
