f369a4f368c17da12f63297af4cf42588bc0ab82
Christian Razo <37232875+Christian-Ra@users.noreply.github.com>
2024-07-08 16:59:23 -0500

    added basic routes and types for diaries

---
+++ Part_9/notes/build/calculator.js
@@ -0,0 +1,33 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.calculator = void 0;
+const calculator = (a, b, op) => {
+    switch (op) {
+        case 'multiply':
+            return a * b;
+        case 'divide':
+            if (b === 0)
+                throw new Error('Can\'t divide by 0!');
+            return a / b;
+        case 'add':
+            return a + b;
+        default:
+            throw new Error('Operation is not multiply, add or divide!');
+    }
+};
+exports.calculator = calculator;
+try {
+    console.log(calculator(1, 5, 'divide'));
+}
+catch (error) {
+    let errorMessage = 'Something went wrong: ';
+    //! here we can not use error.message
+    if (error instanceof Error) {
+        //* the type is narrowed and we can refer to error.message
+        errorMessage += error.message;
+    }
+    //! here we can not use error.message
+    console.log(errorMessage);
+}
+console.log(process.argv);
+calculator(1, 0, 'divide');
